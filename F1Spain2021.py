91217fa2afc07fa840252647ff84c583e219e843
flotr18 <trouilletflorent@outlook.com>
2025-02-10 16:16:02 +0100

    Leclerc & Sainz Collected

--- F1Spain2021.py
+++ F1Spain2021.py
@@ -1,8 +1,12 @@
 import fastf1 as ff1
 import matplotlib.pyplot as mpl
 
-race = ff1.get_session(2019,'Monza','R')
+race = ff1.get_session(2024,'Monza','R')
 race.load()
-print(race.laps.pick_drivers('LEC'))
+
+drivers = ['LEC','SAI']
+
+for driver in drivers: 
+    print(race.laps.pick_drivers(drivers))
 
 
