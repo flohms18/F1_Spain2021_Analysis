d93711bcb6e735cae200e3bf3d6b2774417bd2d6
flotr18 <trouilletflorent@outlook.com>
2025-02-10 15:15:09 +0100

    Collected Laps from Lewis and Maw

--- F1Spain2021.py
+++ F1Spain2021.py
@@ -1,2 +1,18 @@
 import fastf1 as ff1
 import matplotlib as matplotlib
+
+
+
+race = ff1.get_session(2021,'Spain','R')
+
+drivers = ['HAM','VER']
+
+race.load()
+
+for driver in drivers:
+    print(race.laps.pick_drivers(drivers))
+
+
+
+
+    
