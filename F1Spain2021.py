504390e0b8fb82ca5a153dc3a1d8d57d2a88a663
flotr18 <trouilletflorent@outlook.com>
2025-02-10 16:08:21 +0100

    Update F1Spain2021.py

--- F1Spain2021.py
+++ F1Spain2021.py
@@ -1,18 +1,3 @@
 import fastf1 as ff1
-import matplotlib as matplotlib
+import matplotlib.pyplot as mpl
 
-
-
-race = ff1.get_session(2021,'Spain','R')
-
-drivers = ['HAM','VER']
-
-race.load()
-
-for driver in drivers:
-    print(race.laps.pick_drivers(drivers))
-
-
-
-
-    
