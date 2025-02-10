ffcde1db8d5383a089dd3c16c5b1abb18d74fe46
flotr18 <trouilletflorent@outlook.com>
2025-02-10 17:50:12 +0100

    Add Speed & Distance

--- F1Spain2021.py
+++ F1Spain2021.py
@@ -5,5 +5,9 @@ race = ff1.get_session(2023,'Sahkir','Q')
 race.load()
 
 
-MaxTelem = race.laps.pick_drivers('VER').pick_fastest()
-print(MaxTelem['LapTime'])
+MaxSpeed = race.laps.pick_drivers('VER').pick_laps(3).get_car_data()['Speed']
+MaxDistance = race.laps.pick_drivers('VER').pick_laps(3).get_car_data().add_distance()['Distance']
+
+
+print(MaxSpeed)
+print(MaxDistance)
