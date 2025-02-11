7361755cc0c0424b9b3621478b03f1a4c449bfbe
flotr18 <trouilletflorent@outlook.com>
2025-02-11 11:17:42 +0100

    Update ConstructorsChampionship.py

--- ConstructorsChampionship.py
+++ ConstructorsChampionship.py
@@ -4,9 +4,12 @@ import ergast_py
 e = ergast_py.Ergast()
 
 
+
 standings = e.season(2024).round(3).get_constructor_standings()
 
 for constructor in standings:
     print(constructor.season)
+
+
 
 
