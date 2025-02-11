27b9477a2498bce7c772868785515864003703de
flotr18 <trouilletflorent@outlook.com>
2025-02-11 11:11:55 +0100

    Update ConstructorsChampionship.py

--- ConstructorsChampionship.py
+++ ConstructorsChampionship.py
@@ -1,2 +1,12 @@
-import fastf1 as ff1
 import matplotlib.pyplot as plt
+import ergast_py
+
+e = ergast_py.Ergast()
+
+
+standings = e.season(2024).round(3).get_constructor_standings()
+
+for constructor in standings:
+    print(constructor.season)
+
+
