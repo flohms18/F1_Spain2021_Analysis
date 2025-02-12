78fdc75de3813e78f9c597777dc4d4b4e09c622c
flotr18 <trouilletflorent@outlook.com>
2025-02-12 10:33:59 +0100

    Constructors Standings after Round 24

--- ConstructorsChampionship.py
+++ ConstructorsChampionship.py
@@ -3,12 +3,31 @@ import ergast_py
 
 e = ergast_py.Ergast()
 
+plt.figure(figsize=(20, 6))
 
 
 standings = e.season(2024).round(24).get_constructor_standings()
 
+
 for constructor in standings:
     for team in constructor.constructor_standings:
-        print(team.points)
+        TeamName = team.constructor.name
+        TeamPoints = team.points
+        plt.bar(TeamName, TeamPoints, color='blue')
+        plt.gca().invert_yaxis()
+        
+plt.xlabel("Team Name")
+plt.ylabel("Points")
+
+plt.title("F1 24 Constructor Standings after Round 24")
+plt.legend()
+plt.grid(True)
+plt.show()
+        
+        
+
+
+
+
 
 
